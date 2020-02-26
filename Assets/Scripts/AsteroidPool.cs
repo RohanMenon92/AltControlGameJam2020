1ad7405a7bb1dffe480db5f540e7f3238d8ba99c
Thanik <thanik@gmail.com>
2020-02-26 22:45:55 +0000

    Translate all objects in game scene back to 0, 0, 0, spawn asteroids when game starts

--- Assets/Scripts/AsteroidPool.cs
+++ Assets/Scripts/AsteroidPool.cs
@@ -9,16 +9,26 @@ public class AsteroidPool : MonoBehaviour
     public int numOfAsteroids;
     public float spawnTime;
 
+    public float maxX;
+    public float maxZ;
+    public float maxInitialForce;
 
     public List<Asteroid> asteroidPool;
+    public List<Asteroid> unusedAsteroid;
     void Start()
     {
         for(int i = 0; i < numOfAsteroids; i++)
         {
-            GameObject newAsteroid = Instantiate(asteroidPrefabs[Random.Range(0,6)], transform);
+            GameObject newAsteroid = Instantiate(asteroidPrefabs[Random.Range(0, asteroidPrefabs.Count)], transform);
             asteroidPool.Add(newAsteroid.GetComponent<Asteroid>());
-            newAsteroid.SetActive(false);
+
+            // random spawn in map
+
+            Vector3 newPosition = new Vector3(Random.Range(-maxX, maxX), 0, Random.Range(-maxZ, maxZ));
+            newAsteroid.transform.position = newPosition;
+            newAsteroid.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-maxInitialForce, maxInitialForce), 0, Random.Range(-maxInitialForce, maxInitialForce)));
         }
+
     }
 
     // Update is called once per frame
