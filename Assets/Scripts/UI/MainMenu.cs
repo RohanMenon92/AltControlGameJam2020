1c14eed2fbcc3ce37188af584efde0e7392e784e
DESKTOP-2BSF166\Feras <fhathaf@gmail.com>
2020-02-26 20:13:42 +0000

    Added Scripts to control the main menu and spin the ship in x-axis in the main menu. Created the main menu scene with assets from Jerry and made it a prefab. Added both scenes to the build settings. HELP MENU NOT IMPLEMENTED.

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject settingsPanel;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exiting game.");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UI/SpinShipInX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinShipInX : MonoBehaviour
+{
+    Transform ship;
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ship = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ship.Rotate(new Vector3(0, rotationSpeed * Time.deltaTime, 0));
+    }
+}
