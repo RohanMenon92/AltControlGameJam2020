37079b53fec8dd76848899f11a4593899409de66
rohanm92 <randommenon@gmail.com>
2020-02-26 20:01:13 +0000

    Add bullet scripts and sholtgun bullet scripts and gunPort

--- Assets/Scripts/GunPort.cs
+++ Assets/Scripts/GunPort.cs
@@ -9,6 +9,9 @@ public class GunPort : MonoBehaviour
 
     GameManager gameManager;
 
+    public float firePerSeconds;
+    bool canFire = true;
+
     void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
@@ -20,24 +23,57 @@ public class GunPort : MonoBehaviour
         
     }
 
-    public void Fire()
+    public void Fire(bool isEnemy)
     {
-        GameObject newBullet = gameManager.GetBullet(gunType, transform);
-        switch(gunType) {
-            case GameConstants.GunTypes.MachineGun:
-                // Get MachineGun Bullet from GameManager MachineGunBulletPool 
-                // Set transform and set initial velocity of rigidbody component
-                break;
-            case GameConstants.GunTypes.ShotGun:
-                // Get 5-10 ShotGun Bullets from GameManager.ShotGunBulletPool
-                // Give random spread to rotation/initial position
-                // Set Initial Velocity of rigidbody
-                break;
-            case GameConstants.GunTypes.LaserGun:
-                // Create Laser Bullet Prefab and move to position/scale
-                // Enable Laser Object Collider and do damage
-                // Laser Bullet Automatically dissapears and returns to pool
-                break;
+        // Check if it can fire
+        if(canFire)
+        {
+            switch (gunType)
+            {
+                case GameConstants.GunTypes.MachineGun:
+                    BulletScript newBullet = gameManager.GetBullet(gunType, transform).GetComponent<BulletScript>();
+                    newBullet.transform.position = transform.position + transform.forward;
+                    newBullet.transform.rotation = transform.rotation;
+                    newBullet.isEnemyShot = isEnemy;
+                    newBullet.gameObject.SetActive(true);
+                    newBullet.FireBullet();
+                    // Set transform and set initial velocity of rigidbody component
+                    break;
+                case GameConstants.GunTypes.ShotGun:
+                    // Do it for 5 shots
+                    for(int i = 0; i < 6; i++)
+                    {
+                        ShotgunBulletScript newShoutgunBullet = gameManager.GetBullet(gunType, transform).GetComponent<ShotgunBulletScript>();
+                        newShoutgunBullet.transform.position = transform.position + transform.forward;
+                        newShoutgunBullet.transform.eulerAngles =  new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + Random.Range(-10f, 10f), transform.eulerAngles.z);
+                        newShoutgunBullet.isEnemyShot = isEnemy;
+                        newShoutgunBullet.gameObject.SetActive(true);
+                        newShoutgunBullet.FireShotgun();
+                    }
+                    // Get 5-10 ShotGun Bullets from GameManager.ShotGunBulletPool
+                    // Give random spread to rotation/initial position
+                    // Set Initial Velocity of rigidbody
+                    break;
+                case GameConstants.GunTypes.LaserGun:
+                    LaserBulletScript newLaserBullet = gameManager.GetBullet(gunType, transform).GetComponent<LaserBulletScript>();
+                    newLaserBullet.transform.position = transform.position + transform.forward;
+                    newLaserBullet.transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + Random.Range(-5f, 5f), transform.eulerAngles.z);
+                    newLaserBullet.isEnemyShot = isEnemy;
+                    newLaserBullet.gameObject.SetActive(true);
+                    newLaserBullet.FireLaser();
+
+                    break;
+            }
+            // Cannot fire and start Coroutine to be able to fire next
+            canFire = false;
+            StartCoroutine(ResetCanFire());
         }
+    }
+
+    IEnumerator ResetCanFire()
+    {
+        // Wait for firePerSeconds and the say canFire is true 
+        yield return new WaitForSeconds(firePerSeconds);
+        canFire = true;
     }
 }
--- Assets/Scripts/ShotgunBulletScript.cs
+++ Assets/Scripts/ShotgunBulletScript.cs
@@ -1,18 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShotgunBulletScript : MonoBehaviour
 {
+    // Can be part of a "Bullet" Super Class
+    public bool isEnemyShot;
+    public float damage;
+    public float bulletSpeed;
+
+    GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = FindObjectOfType<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void ReturnBulletToPool()
+    {
+        // Return Bullet
+        gameManager.ReturnBulletToPool(gameObject, GameConstants.GunTypes.ShotGun);
+    }
+
+    internal void OnHit()
+    {
+        ReturnBulletToPool();
+    }
+
+    // Called when player refelects a shot
+    internal void OnShield(Vector3 impactPoint)
+    {
+        isEnemyShot = false;
+        this.GetComponent<Rigidbody>().velocity = (transform.position - impactPoint).normalized * bulletSpeed;
+    }
+
+    internal void FireShotgun()
+    {
+        this.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed, ForceMode.VelocityChange);
     }
 }
