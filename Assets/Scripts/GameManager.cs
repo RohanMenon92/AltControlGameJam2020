52f30dadfee02bbc045856c826dbc55e5ec68609
rohanm92 <randommenon@gmail.com>
2020-02-26 05:54:03 +0000

    Add commit for player control smoothening(smoothens fluctuations from input) and add GunPort Logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,29 +5,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [Range(-0.5f, 0.5f)]
-    public float currThrust;
 
-    [Range(-0.5f,0.5f)]
-    public float currRudderAngle;
-
-    [Range(-0.5f, 0.5f)]
-    public float currAimAngle;
-
-    [Range(-0.5f, 0.5f)]
-    public float currPreciseAimAngle;
-
-    public float shipSpeed;
+    // public player
+    public PlayerScript player;
 
     bool isRechargePressed;
     bool isFiring;
     bool isShielding;
-
-    public Transform ship;
-    public Transform shipShield;
-
-
-    // public player
 
     // Start is called before the first frame update
     void Start()
@@ -37,46 +21,29 @@ public class GameManager : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-
-//        ship.localEulerAngles = new Vector3(0f, ship.localEulerAngles.y + currRudderAngle * 200 * Time.deltaTime, 0f);
-        ship.localEulerAngles = new Vector3(0f, currRudderAngle * 360, 0f);
-
-        shipShield.localEulerAngles = new Vector3(0f, currAimAngle * 360, 0f);
+    {      
 
 
-        if (currThrust > 0)
-        {
-            //PlayerPrefs.AddForce(transform.forward * currThrust);
-        } else
-        {
-            //PlayerPrefs.velocity -= currThrust;
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        ship.GetComponentInParent<Rigidbody>().AddForce(ship.forward * currThrust * shipSpeed, ForceMode.VelocityChange);
     }
 
     internal void UpdateThrustInput(float val)
     {
-        currThrust = val - 0.5f;
+        player.currThrust = val - 0.5f;
     }
 
     internal void UpdateRudderAngle(float val)
     {
-        currRudderAngle = val - 0.5f;
+        player.currRudderAngle = val - 0.5f;
     }
 
     internal void UpdateAimAngle(float val)
     {
-        currAimAngle = val - 0.5f;
+        player.currAimAngle = val - 0.5f;
     }
 
     internal void UpdatePreciseAimAngle(float val)
     {
-        currPreciseAimAngle = val - 0.5f;
+        player.currPreciseAimAngle = val - 0.5f;
     }
 
     internal void UpdateRechargeButton(bool val)
@@ -93,7 +60,7 @@ public class GameManager : MonoBehaviour
 
     private void OnRechargeActivate()
     {
-        Debug.Log("RECHARGE ACTIVATED!!");
+        player.OnRecharge();
     }
 
     internal void UpdateFireButton(bool val)
@@ -110,7 +77,7 @@ public class GameManager : MonoBehaviour
 
     private void OnFireActivate()
     {
-        Debug.Log("FIRE ACTIVATED");
+        player.OnFire();
     }
 
     internal void UpdateShieldButton(bool val)
@@ -127,6 +94,6 @@ public class GameManager : MonoBehaviour
 
     private void OnShieldActivate()
     {
-        Debug.Log("SHIELD ACTIVATED");
+        player.OnShield();
     }
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,9 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    [Range(-0.5f, 0.5f)]
+    public float currThrust;
+
+    [Range(-0.5f, 0.5f)]
+    public float currRudderAngle;
+
+    [Range(-0.5f, 0.5f)]
+    public float currAimAngle;
+
+    [Range(-0.5f, 0.5f)]
+    public float currPreciseAimAngle;
+
+    public float shipSpeed;
+
+    public Transform shipShield;
+
+    public List<GunPort> gunPorts;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +33,47 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Rotate Ship
+        transform.localEulerAngles = AngleLerp(transform.localEulerAngles, new Vector3(0f, currRudderAngle * 358, 0f), Time.deltaTime);
+
+        // Rotate Ship by rate
+        shipShield.localEulerAngles = AngleLerp(shipShield.localEulerAngles, new Vector3(0f, currAimAngle * 358, 0f), Time.deltaTime);
+    }
+
+    Vector3 AngleLerp(Vector3 StartAngle, Vector3 FinishAngle, float t)
+    {
+        //float xLerp = Mathf.LerpAngle(StartAngle.x, FinishAngle.x, t);
+        float yLerp = Mathf.LerpAngle(StartAngle.y, FinishAngle.y, t);
+        //float zLerp = Mathf.LerpAngle(StartAngle.z, FinishAngle.z, t);
+        //Vector3 Lerped = new Vector3(xLerp, yLerp, zLerp);
+        Vector3 Lerped = new Vector3(0, yLerp, 0);
+        return Lerped;
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * currThrust * shipSpeed, ForceMode.VelocityChange);
+    }
+    void FireCannons()
+    {
+        foreach(GunPort gun in gunPorts)
+        {
+            gun.Fire();
+        }
+    }
+
+    internal void OnShield()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void OnRecharge()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void OnFire()
+    {
+        throw new NotImplementedException();
     }
 }
