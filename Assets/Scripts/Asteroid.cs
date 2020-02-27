210cf841b879415c198bd172be74210f949ffdf7
Thanik <thanik@gmail.com>
2020-02-27 00:31:37 +0000

    self-destroy asteroid when it gone out of range, destroy asteroid when the bullet hit

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -4,29 +4,54 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    
+    public bool testHit;
+    private ParticleSystem particle;
+    private MeshRenderer renderer;
+    private AsteroidPool pool;
     void Start()
     {
-        
+        particle = GetComponent<ParticleSystem>();
+        pool = FindObjectOfType<AsteroidPool>();
+        renderer = GetComponent<MeshRenderer>();
+        particle.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(testHit)
+        {
+            OnHit();
+            testHit = false;
+        }
     }
 
     public void OnHit()
     {
         // play explosion animation
         
+        particle.Play();
+        StartCoroutine(waitForAnimation());
+
     }
 
-    public void OnCollisionEnter(Collision collision)
+    IEnumerator waitForAnimation()
     {
-        if(collision.gameObject.CompareTag("Bullet"))
-        {
+        yield return new WaitForSeconds(0.15f);
+        renderer.enabled = false;
+        yield return new WaitForSeconds(0.25f);
+        particle.Stop();
+        yield return new WaitForSeconds(2.0f);
+        pool.unusedAsteroid.Add(this);
+        gameObject.SetActive(false);
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Bullet"))
+        {
+            OnHit();
+            other.gameObject.GetComponent<BulletScript>().OnHit();
         }
     }
 }
--- Assets/Scripts/AsteroidPool.cs
+++ Assets/Scripts/AsteroidPool.cs
@@ -13,6 +13,8 @@ public class AsteroidPool : MonoBehaviour
     public float maxZ;
     public float maxInitialForce;
 
+    public int unusedAsteroidThreshold;
+
     public List<Asteroid> asteroidPool;
     public List<Asteroid> unusedAsteroid;
     void Start()
@@ -34,11 +36,36 @@ public class AsteroidPool : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (unusedAsteroid.Count >= unusedAsteroidThreshold)
+        {
+            for (int i = 0; i < unusedAsteroid.Count; i++)
+            {
+                reuseAsteroid(unusedAsteroid[i]);
+            }
+            unusedAsteroid.Clear();
+        }
+
+        foreach(Asteroid asteroid in asteroidPool)
+        {
+            if(asteroid.gameObject.activeSelf)
+            {
+                if (asteroid.transform.position.x < -maxX || asteroid.transform.position.x > maxX || asteroid.transform.position.z < -maxZ || asteroid.transform.position.z > maxZ)
+                {
+                    asteroid.OnHit();
+                }
+            }
+        }
     }
 
-    void spawnNewAsteroid()
+    void reuseAsteroid(Asteroid asteroid)
     {
+        asteroid.gameObject.SetActive(true);
+        Vector3 newPosition = new Vector3(Random.Range(-maxX, maxX), 0, Random.Range(-maxZ, maxZ));
+        asteroid.transform.position = newPosition;
+        asteroid.GetComponent<ParticleSystem>().Stop();
+        asteroid.GetComponent<MeshRenderer>().enabled = true;
+        asteroid.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        asteroid.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-maxInitialForce, maxInitialForce), 0, Random.Range(-maxInitialForce, maxInitialForce)));
 
     }
 }
